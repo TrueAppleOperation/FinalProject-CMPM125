a8d503b9ab034dbf97528911d8ca4acc5bf3e4f9
romeomore <rmoren15@ucsc.edu>
2025-12-02 21:12:33 -0800

    Fixed Sun Power

--- Assets/Scripts/SunProjectile.cs
+++ Assets/Scripts/SunProjectile.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class SunProjectile : MonoBehaviour
 {
-    public float offsetDistance = 1.5f;   
+    public float offsetDistance = 1.5f;
 
     private PlayerController player;
-    private Vector2 lastDir = Vector2.up; 
+    private Vector2 lastDir = Vector2.up;
+
     public void Setup(Vector2 dir, float speed)
     {
-       
         player = FindObjectOfType<PlayerController>();
         if (player == null)
         {
@@ -17,7 +17,6 @@ public class SunProjectile : MonoBehaviour
             return;
         }
 
-        
         if (dir.sqrMagnitude > 0.01f)
             lastDir = dir.normalized;
     }
@@ -26,7 +25,13 @@ public class SunProjectile : MonoBehaviour
     {
         if (player == null) return;
 
-        
+        // Destroy when mouse is released
+        if (!Input.GetMouseButton(0))
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Vector2 dir = player.LastMoveDir;
 
         if (dir.sqrMagnitude < 0.01f)
@@ -34,13 +39,13 @@ public class SunProjectile : MonoBehaviour
         else
             lastDir = dir.normalized;
 
-        
         if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
-            dir = new Vector2(Mathf.Sign(dir.x), 0f);   
+            dir = new Vector2(Mathf.Sign(dir.x), 0f);
         else
-            dir = new Vector2(0f, Mathf.Sign(dir.y));  
+            dir = new Vector2(0f, Mathf.Sign(dir.y));
 
         Vector3 offset = (Vector3)dir * offsetDistance;
         transform.position = player.transform.position + offset;
     }
 }
+        transform.position = player.transform.position + offset;
--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public enum SwordType
 {
     None,
@@ -15,85 +16,102 @@ public class Sword : MonoBehaviour
     private SunProjectile activeSun; 
 
     [SerializeField] GameObject windTornadoPrefab;
-    [SerializeField] GameObject lightningPrefab;
-    [SerializeField] GameObject sunRayPrefab;
-    [SerializeField] GameObject snowFreezePrefab;
+    using UnityEngine;
 
-    public void SetSwordType(Vector2 direction)
+    public enum SwordType { None, Wind, Rain, Sun, Snow }
+
+    public class Sword : MonoBehaviour
     {
-        switch (currentType)
+        public SwordType currentType = SwordType.None;
+        private SunProjectile activeSun;
+
+        [SerializeField] GameObject windTornadoPrefab;
+        [SerializeField] GameObject lightningPrefab;
+        [SerializeField] GameObject sunRayPrefab;
+        [SerializeField] GameObject snowFreezePrefab;
+
+        public void SetSwordType(Vector2 direction)
         {
-            case SwordType.Wind:
-                ShootTornado(direction);
-                break;
+            switch (currentType)
+            {
+                case SwordType.Wind:
+                    ShootTornado(direction);
+                    break;
 
-            case SwordType.Rain:
-                ThunderStrike(direction);
-                break;
+                case SwordType.Rain:
+                    ThunderStrike(direction);
+                    break;
 
-            case SwordType.Sun:
-                SunRay(direction);
-                break;
+                case SwordType.Sun:
+                    SunRay(direction);
+                    break;
 
-            case SwordType.Snow:
-                SnowFreeze(direction);
-                break;
+                case SwordType.Snow:
+                    SnowFreeze(direction);
+                    break;
+            }
+        }
+
+        // debug helper so pressing P always spawns a sun ray
+        public void DebugSpawnSun(Vector2 direction)
+        {
+            Debug.Log("DebugSpawnSun called");
+            SunRay(direction);
+        }
+
+        void ShootTornado(Vector2 direction)
+        {
+            GameObject tornado = Instantiate(windTornadoPrefab, transform.position, Quaternion.identity);
+            tornado.GetComponent<TornadoProjectile>().Setup(direction, 8f);
+        }
+
+        void ThunderStrike(Vector2 direction)
+        {
+            Debug.Log("Lighting Prefab: " + lightningPrefab);
+            GameObject lightning = Instantiate(lightningPrefab, transform.position, Quaternion.identity);
+            lightning.GetComponent<LightningProjectile>().Setup(direction, 6f);
+        }
+
+        void SunRay(Vector2 direction)
+        {
+            Debug.Log("SunRay() called, dir = " + direction);
+
+            if (activeSun != null)
+            {
+                Debug.Log("SunRay: sunPower already active, skipping spawn.");
+                return;
+            }
+
+            if (sunRayPrefab == null)
+            {
+                Debug.LogError("SunRay: sunRayPrefab is NULL!");
+                return;
+            }
+
+            GameObject sunRay = Instantiate(sunRayPrefab, transform.position, Quaternion.identity);
+            SunProjectile sp = sunRay.GetComponent<SunProjectile>();
+            if (sp == null)
+            {
+                Debug.LogError("SunRay: SunProjectile component missing on prefab");
+                Destroy(sunRay);
+                return;
+            }
+
+            sp.Setup(direction, 5f);
+            activeSun = sp;
+            StartCoroutine(ClearSunWhenDestroyed(sunRay));
+        }
+
+        private System.Collections.IEnumerator ClearSunWhenDestroyed(GameObject sunRay)
+        {
+            while (sunRay != null)
+                yield return null;
+            activeSun = null;
+        }
+
+        void SnowFreeze(Vector2 direction)
+        {
+            GameObject snowFreeze = Instantiate(snowFreezePrefab, transform.position, Quaternion.identity);
+            snowFreeze.GetComponent<SnowProjectile>().Setup(direction, 4f);
         }
     }
-
-    // debug helper so pressing P always spawns a sun ray
-    public void DebugSpawnSun(Vector2 direction)
-    {
-        Debug.Log("DebugSpawnSun called");
-        SunRay(direction);
-    }
-
-    void ShootTornado(Vector2 direction)
-    {
-        GameObject tornado = Instantiate(windTornadoPrefab, transform.position, Quaternion.identity);
-        tornado.GetComponent<TornadoProjectile>().Setup(direction, 8f);
-    }
-
-    void ThunderStrike(Vector2 direction)
-    {
-        Debug.Log("Lighting Prefab: " + lightningPrefab);
-        GameObject lightning = Instantiate(lightningPrefab, transform.position, Quaternion.identity);
-        lightning.GetComponent<LightningProjectile>().Setup(direction, 6f);
-    }
-
-   void SunRay(Vector2 direction)
-{
-    Debug.Log("SunRay() called, dir = " + direction);
-
-    if (activeSun != null)
-    {
-        Debug.Log("SunRay: sunPower already active, skipping spawn.");
-        return;
-    }
-
-    GameObject sunRay = Instantiate(sunRayPrefab, transform.position, Quaternion.identity);
-    if (sunRay == null)
-    {
-        Debug.LogError("SunRay: sunRayPrefab is NULL!");
-        return;
-    }
-
-    SunProjectile sp = sunRay.GetComponent<SunProjectile>();
-    if (sp == null)
-    {
-        Debug.LogError("SunRay: SunProjectile component missing on prefab");
-        return;
-    }
-
-    sp.Setup(direction, 5f);
-    activeSun = sp;
-}
-
-
-
-    void SnowFreeze(Vector2 direction)
-    {
-        GameObject snowFreeze = Instantiate(snowFreezePrefab, transform.position, Quaternion.identity);
-        snowFreeze.GetComponent<SnowProjectile>().Setup(direction, 4f);
-    }
-}
