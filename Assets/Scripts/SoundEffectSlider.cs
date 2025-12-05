f4017d6fbe2a16088385a42d401147989cbce8bf
Fishbowl <icecoffee1945@gmail.com>
2025-12-05 14:02:03 -0800

    Fixed SFX slider

--- Assets/Scripts/SoundEffectSlider.cs
+++ Assets/Scripts/SoundEffectSlider.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class SoundEffectSlider : MonoBehaviour
 {
-    public AudioSource sfxSource;
     public Slider sfxSlider;
 
     private void Start()
     {
         float savedVolume = PlayerPrefs.GetFloat("SFXVolume", 1f);
-
         sfxSlider.value = savedVolume;
-        sfxSource.volume = savedVolume;
+        if (SoundFXManager.instance != null)
+        {
+            SoundFXManager.instance.SetVolume(savedVolume);
+        }
+
         sfxSlider.onValueChanged.AddListener(OnVolumeChange);
     }
+
     private void OnVolumeChange(float value)
     {
-        sfxSource.volume = value;
         PlayerPrefs.SetFloat("SFXVolume", value);
+        PlayerPrefs.Save();
+
+        if (SoundFXManager.instance != null)
+        {
+            SoundFXManager.instance.SetVolume(value);
+        }
     }
 }
--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -4,31 +4,34 @@ public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager instance;
     [SerializeField] private AudioSource soundFXObject;
-    [Range(0f, 1f)] public float volume = 1f;   // Add this
+    [Range(0f, 1f)] public float volume = 1f;
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+            volume = PlayerPrefs.GetFloat("SFXVolume", 1f);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
     public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform)
     {
-        // Create new AudioSource at position
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
-
-        // Assign the audioClip
         audioSource.clip = audioClip;
-
-        // Assign volume
         audioSource.volume = volume;
-
-        // Play
         audioSource.Play();
-
-        // Destroy after clip ends
         Destroy(audioSource.gameObject, audioSource.clip.length);
+    }
+    public void SetVolume(float newVolume)
+    {
+        volume = newVolume;
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
     }
 }
